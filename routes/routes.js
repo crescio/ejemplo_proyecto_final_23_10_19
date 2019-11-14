6b924f2df9a68445a7b129bf51e0f4210c3580dc
Crescio <paulo.crescionini@gmail.com>
2019-11-13 22:00:53 -0300

    push

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,15 @@ router.post('/classify/image', watsonVisualRecongnitionController.classifyImage)
 router.post('/search/discovery', watsonDiscoveryController.query);
 
 router.post('/test/webhook/assistant',(req,res) =>{
-    console.log(req.body);
+    if(req.body.action === 'reservar_hotel'){
+        let habitaciones = [11,23,54,32,26];
+        res.send({
+            success: true,
+            mesage: 'habitacion reservada',
+            date: req.body.date,
+            habitacion: RadioNodeList(habitaciones)
+        })
+    }
     res.send({response: "hola webhook"});
 });
 
